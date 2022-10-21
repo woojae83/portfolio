6a7564cd1a4d46caf4110640c3b766dc245a0ada
Jeonwoojae <108383150+woojae83@users.noreply.github.com>
2022-10-21 16:25:06 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let mainText = document.querySelector("h1");
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+
+    if (value > 150) {
+        mainText.style.animation = "disappear 1s ease-out forwards";
+    }else{
+        mainText.style.animation = `slide 1s ease-out`;
+    }
+   
+   
+});
